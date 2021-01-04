b43d839ea3159722850aaa356bd5e275f9907a90
Manal Shehri <manal.shehri.20@gmail.com>
2021-01-04 12:09:03 +0300

    add designer and post models

--- models/post.js
+++ models/post.js
@@ -4,7 +4,10 @@ const postScema = new mongoose.Schema({
     content :String ,
     img :String ,
     description: String, 
-    details : String
+    details : String,
+    // one to many 
+    user :{type : mongoose.Schema.Types.ObjectId , ref : 'user' }
+    
 } , {timestamps :true})
 
 const Post = mongoose.model('post' , postScema)
